25f6fae2dd2e74444f8ee919c4a580b0632e5249
Nick Pai <npai.nyc@gmail.com>
2021-03-31 08:27:42 -0400

    (WIP): MVP Voting subgraph, re-use Voting handlers for VotingAncillary ABI

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,12 @@
 export { handleTransferVotingToken } from "./mappings/votingToken";
 
 export {
-  handleEncryptedVote,
   handleVoteRevealed,
   handleVoteCommitted,
   handlePriceRequestAdded,
   handlePriceResolved,
   handleRewardsRetrieved
 } from "./mappings/voting";
-
-export {
-  handleEncryptedVote as handleEncryptedVoteAncillary,
-  handleVoteRevealed as handleVoteRevealedAncillary,
-  handleVoteCommitted as handleVoteCommittedAncillary,
-  handlePriceRequestAdded as handlePriceRequestAddedAncillary,
-  handlePriceResolved as handlePriceResolvedAncillary,
-  handleRewardsRetrieved as handleRewardsRetrievedAncillary
-} from "./mappings/votingAncillary";
 
 export {
   handleSupportedIdentifierAdded,
--- src/mappings/voting.ts
+++ src/mappings/voting.ts
@@ -1,5 +1,4 @@
 import {
-  EncryptedVote,
   PriceRequestAdded,
   PriceResolved,
   VoteCommitted,
@@ -28,39 +27,11 @@ import {
 
 import { log, BigInt, BigDecimal } from "@graphprotocol/graph-ts";
 
-// - event: EncryptedVote(indexed address,indexed uint256,indexed bytes32,uint256,bytes)
-//   handler: handleEncryptedVote
-//  event EncryptedVote(
-//      address indexed voter,
-//      uint256 indexed roundId,
-//      bytes32 indexed identifier,
-//      uint256 time,
-//      bytes encryptedVote
-//  );
-
-export function handleEncryptedVote(event: EncryptedVote): void {
-  let voteId = event.params.voter
-    .toHexString()
-    .concat("-")
-    .concat(event.params.identifier.toString())
-    .concat("-")
-    .concat(event.params.time.toString())
-    .concat("-")
-    .concat(event.params.roundId.toString());
-  let vote = getOrCreateCommittedVote(voteId);
-
-  vote.encryptedVoteData = event.params.encryptedVote;
-
-  vote.save();
-}
-
 // - event: PriceRequestAdded(indexed uint256,indexed bytes32,uint256)
 //   handler: handlePriceRequestAdded
 //  event PriceRequestAdded(uint256 indexed roundId, bytes32 indexed identifier, uint256 time);
 
 export function handlePriceRequestAdded(event: PriceRequestAdded): void {
-  log.warning("New PriceRequestAdded event, identifier {}", [event.params.identifier.toString()]);
-
   let requestId = event.params.identifier
     .toString()
     .concat("-")
